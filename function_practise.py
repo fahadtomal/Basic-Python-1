e5da61fb0b9ea2be85b82387b2b00dbbeff37d76
Mostaque Fahad Tomal <fahadtomal@users.noreply.github.com>
2016-09-13 20:17:45 +0600

    Add files via upload

---
+++ function_practise.py
@@ -0,0 +1,15 @@
+def tomal(x,y):
+    print(x+y);
+    print(x-y);
+    print(x*y);
+    print(x/y);
+ 
+ 
+ 
+def tomal_1():
+     print("Hello,world");
+     
+     
+     
+def tomal_2(x,y,):
+    return x+y;
